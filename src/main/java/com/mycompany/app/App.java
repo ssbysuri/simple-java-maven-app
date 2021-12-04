4109fad4ada8fab233e3542893a8988abd5898c8
ssbysuri <94780113+ssbysuri@users.noreply.github.com>
2021-12-04 21:53:17 +0530

    Update App.java
    update by ssby

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        system.out.println("ssss");
     }
 
     private final String getMessage() {
